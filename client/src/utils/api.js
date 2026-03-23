9d9633f85eabacbd3f0079018968fcedca06aa17
Matt O. Morales <idsilmat@gmail.com>
2026-03-23 17:05:42 +0800

    Fix:Socket

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,11 +1,11 @@
 import io from 'socket.io-client';
 import axios from 'axios';
 
-const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || 'http://localhost:5001';
+//const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || 'http://localhost:5001';
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5001/thoughts';
 
 export const socket = io("https://thought-classifier-571429268886.us-central1.run.app", {
-    transports: ["websocket"], // 👈 THIS IS THE MAGIC LINE
+    transports: ["websocket"], 
     upgrade: false
 });
 export const api = axios.create({ baseURL: API_URL });
